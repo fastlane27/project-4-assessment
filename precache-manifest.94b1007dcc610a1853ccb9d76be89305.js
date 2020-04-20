bbadf8d18f56485060277f1e4122f49eb4c87b06
David Lane <fastlane27@gmail.com>
2020-04-20 10:16:43 -0500

    Updates

---
+++ precache-manifest.94b1007dcc610a1853ccb9d76be89305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12c45a8d928a7dbb0fc13ae9b687ea0",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "235bc76d0d2e6f3d67dd",
+    "url": "/project-4-assessment/static/css/main.5621ada5.chunk.css"
+  },
+  {
+    "revision": "47fe67cf337cd4770098",
+    "url": "/project-4-assessment/static/js/2.ec989648.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.ec989648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235bc76d0d2e6f3d67dd",
+    "url": "/project-4-assessment/static/js/main.33852423.chunk.js"
+  },
+  {
+    "revision": "bb8b828653ad1446ab40",
+    "url": "/project-4-assessment/static/js/runtime-main.58f97c80.js"
+  }
+]);
